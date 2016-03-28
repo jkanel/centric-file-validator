a54262b3ea11bb5af7c18e62317912297aa4184a
jkanel <kanel.jeff@gmail.com>
2016-03-28 15:31:23 -0400

    Update

--- Centric.DNA.FileValidator/Properties/AssemblyInfo.cs
+++ Centric.DNA.FileValidator/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Centric.DNA.FileValidator")]
-[assembly: AssemblyDescription("Centric File Validator")]
+[assembly: AssemblyTitle("Centric File Validator")]
+[assembly: AssemblyDescription("Centric Data & Analytics File Validator")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Centric Consulting")]
 [assembly: AssemblyProduct("Centric.DNA.FileValidator")]
